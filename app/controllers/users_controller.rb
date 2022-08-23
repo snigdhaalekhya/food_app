6543d3bc074b5722629e90d27302abc8ec299d6c
snigdha_alekhya <pvn.snigdhaalekhya@gmail.com>
2022-08-23 12:36:58 +0530

    changes by snigdhaalekhya

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    # skip_before_action :ensure_user_logged_in
+
+    def new
+    end
+
+    def create
+     user=User.find_by(mobile_no: params[:mobile_no])
+     if user
+         flash[:error]="This mobile number is already registered. Please retry."
+         #redirect_to new_user_path 
+     else
+         user=User.create!(
+            name: params[:name],
+            mobile_no: params[:mobile_no],
+            password: params[:password],
+            address: params[:address],
+         )
+        #  session[:current_user_id]= user.id
+        #    if user.save
+        #       redirect_to "/" 
+        #     else
+        #        flash[:error]= user.errors.full_messages.join(", ")
+        #        redirect_to new_user_path
+        #     end
+        end
+    end
+end
